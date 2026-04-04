ce757f2d11de4b84fb34324311c77ed4ee585ce9
Guntereno <petegunter@gmail.com>
2026-04-04 10:18:28 +0100

    Update UserInterface to use MonoGame.Extended camera

--- FlappyBird.Core/UserInterface.cs
+++ FlappyBird.Core/UserInterface.cs
@@ -2,8 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Momo.Ui;
-using Momo.Graphics;
-using System.ComponentModel;
+using MonoGame.Extended;
 
 namespace FlappyBird.Core;
 
@@ -17,7 +16,8 @@ public class UserInterface : DrawableGameComponent
 
     private SpriteFont? _font = null;
 
-    private Rectangle _bounds;
+    private OrthographicCamera? _camera;
+
 
     private TextBox _introText = new TextBox();
     private TextBox _scoreLabel = new TextBox();
@@ -33,9 +33,62 @@ public class UserInterface : DrawableGameComponent
 
     private int _score = 0;
 
+    private Rectangle CameraBounds
+    {
+        get
+        {
+            if(_camera == null)
+            {
+                throw new Exception("Camera not initialised.");
+            }
+
+            return _camera.BoundingRectangle.ToRectangle();
+        }
+    }
+
+    public override void Initialize()
+    {
+        OnViewportUpdated(Game.GraphicsDevice);
+
+        base.Initialize();
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        if (_spriteBatch == null)
+            throw new Exception("LoadContent must be called before drawing!");
+
+        _spriteBatch.Begin();
+
+        switch (_state)
+        {
+            case GameWorld.State.Intro:
+                _introText.Draw(_spriteBatch);
+                break;
+            case GameWorld.State.Gameplay:
+                _scoreLabel.Draw(_spriteBatch);
+                _scoreValue.Draw(_spriteBatch);
+                _highScoreLabel.Draw(_spriteBatch);
+                _highScoreValue.Draw(_spriteBatch);
+                break;
+            case GameWorld.State.GameOver:
+                _gameOverText.Draw(_spriteBatch);
+                break;
+        }
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+    public void OnViewportUpdated(GraphicsDevice graphicsDevice)
+    {
+        _camera = new OrthographicCamera(graphicsDevice);
+        UpdateLayout();
+    }
+
     internal UserInterface(Game game, GameWorld gameWorld) : base(game)
     {
-        CalculateBounds(game.GraphicsDevice);
     }
 
     internal void SetScore(int score)
@@ -94,66 +147,27 @@ public class UserInterface : DrawableGameComponent
         textBox.Scale = 1.0f;
     }
 
-    public override void Draw(GameTime gameTime)
-    {
-        if (_spriteBatch == null)
-            throw new Exception("LoadContent must be called before drawing!");
-
-        _spriteBatch.Begin();
-
-        switch (_state)
-        {
-            case GameWorld.State.Intro:
-                _introText.Draw(_spriteBatch);
-                break;
-            case GameWorld.State.Gameplay:
-                _scoreLabel.Draw(_spriteBatch);
-                _scoreValue.Draw(_spriteBatch);
-                _highScoreLabel.Draw(_spriteBatch);
-                _highScoreValue.Draw(_spriteBatch);
-                break;
-            case GameWorld.State.GameOver:
-                _gameOverText.Draw(_spriteBatch);
-                break;
-        }
-
-        _spriteBatch.End();
-
-        base.Draw(gameTime);
-    }
-
-    public void OnViewportUpdated(GraphicsDevice graphicsDevice)
-    {
-        CalculateBounds(graphicsDevice);
-    }
-
-    private void CalculateBounds(GraphicsDevice graphicsDevice)
-    {
-        int screenWidth = graphicsDevice.Viewport.Width;
-        int screenHeight = graphicsDevice.Viewport.Height;
-
-        _bounds = new Rectangle(0, 0, screenWidth, screenHeight);
-    }
-
     private void UpdateLayout()
     {
-        _introText.DrawRect = _bounds;
+        Rectangle bounds = CameraBounds;
+
+        _introText.DrawRect = bounds;
 
         const int BORDER = 20;
         const int ELEMENT_WIDTH = 300;
         const int LABEL_HEIGHT = 60;
         const int VALUE_HEIGHT = 100;
 
-        int scoreLeft = _bounds.Center.X - (ELEMENT_WIDTH / 2);
+        int scoreLeft = bounds.Center.X - (ELEMENT_WIDTH / 2);
         _scoreLabel.DrawRect = new Rectangle(scoreLeft, BORDER, ELEMENT_WIDTH, LABEL_HEIGHT);
         _scoreValue.DrawRect = new Rectangle(scoreLeft, _scoreLabel.DrawRect.Bottom, ELEMENT_WIDTH, VALUE_HEIGHT);
 
-        int highScoreLeft = _bounds.Right - BORDER - ELEMENT_WIDTH;
+        int highScoreLeft = bounds.Right - BORDER - ELEMENT_WIDTH;
 
         _highScoreLabel.DrawRect = new Rectangle(highScoreLeft, BORDER, ELEMENT_WIDTH, LABEL_HEIGHT);
         _highScoreValue.DrawRect = new Rectangle(highScoreLeft, _highScoreLabel.DrawRect.Bottom, ELEMENT_WIDTH, VALUE_HEIGHT);
 
-        _gameOverText.DrawRect = _bounds;
+        _gameOverText.DrawRect = bounds;
     }
 
     internal void HandleGameWorldStateChanged(GameWorld.State state)
